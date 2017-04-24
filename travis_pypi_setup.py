77925951d4699f3ab32fe04643bbf6f96a02cdbf
Julien Palard <julien@palard.fr>
2017-04-24 13:48:22 +0200

    Add pypi password to .travis.yml

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -21,7 +21,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'julienpalard/textunwrap'
+GITHUB_REPO = 'JulienPalard/textunwrap'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
