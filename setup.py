2bd690e0d3160861ee978e62ba84633a1cc3219f
Julien Palard <julien@palard.fr>
2017-04-25 23:01:24 +0200

    Proofreading setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 from setuptools import setup
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-requirements = []
-
-test_requirements = []
 
 setup(
     name='textunwrap',
@@ -30,8 +26,8 @@ setup(
         ]
     },
     include_package_data=True,
-    install_requires=requirements,
-    license="MIT license",
+    install_requires=[],
+    license="MIT",
     zip_safe=False,
     keywords='textunwrap',
     classifiers=[
@@ -39,14 +35,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=[]
 )
